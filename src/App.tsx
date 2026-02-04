29063273a74e7ad295c41b73d29178f5504c8b40
Gabriel Vasilachi <gabrielvasilachi3@gmail.com>
2026-02-05 00:18:52 +0200

    Initialised everything to zero, fresh new start, all files reset to new incase of anything

--- src/App.tsx
+++ src/App.tsx
@@ -21,71 +21,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-          <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-          </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p><p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-        <p>
-          Edit <code>src/App.tsx</code> Test 1
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> Test 1
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> Test 1
         </p>
       </div>
       <p className="read-the-docs">
