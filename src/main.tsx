e4698288375fe0978936d6bc2d018ea10fbb0cf6
Gabriel Vasilachi <gabrielvasilachi3@gmail.com>
2026-02-05 14:10:57 +0200

    implemented reroutes for pages auth create acc, dashboard, landing, signup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import AuthentificationPage from './pages/authentification'
+import CreateAccountPage from './pages/create-account'
+import LandingPage from './pages/landing'
+import SignUpPage from './pages/signup'
+import Dashboard from './pages/dashboard.tsx'
 
+//functie pentru adaugare de route-s si de chemare a paginilor
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/landing', element: <LandingPage /> },
+  { path: '/authentification', element: <AuthentificationPage /> },
+  { path: '/signup', element: <SignUpPage /> },
+  { path: '/create-account', element: <CreateAccountPage /> },
+  { path: '/dashboard', element: <Dashboard /> },
+])
+
+// folosirea functiei create numita router pentru a chema paginile de mai sus
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/create-account.tsx
@@ -0,0 +1,7 @@
+export default function CreateAccountPage() {
+  return (
+    <div>
+      <h1>Welcome to Create Account Page</h1>
+    </div>
+  )
+}
