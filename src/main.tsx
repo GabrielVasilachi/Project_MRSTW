85c9259bbdf80d8cba8c3b35ae33b7a3ddc3440c
{{your_name}} <{{your_email}}>
2026-02-07 21:57:49 +0200

    Installed tailwind properly and made the header

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import AuthentificationPage from './pages/authentification'
-import CreateAccountPage from './pages/create-account'
-import LandingPage from './pages/landing'
-import SignUpPage from './pages/signup'
-import Dashboard from './pages/dashboard.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/landing.tsx'
+import AuthenticationPage from './pages/authentification.tsx'
+import SignupPage from './pages/signup.tsx'
+import CreateAccountPage from './pages/create-account.tsx'
+import DashboardPage from './pages/dashboard.tsx'
 
-//functie pentru adaugare de route-s si de chemare a paginilor
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/landing', element: <LandingPage /> },
-  { path: '/authentification', element: <AuthentificationPage /> },
-  { path: '/signup', element: <SignUpPage /> },
-  { path: '/create-account', element: <CreateAccountPage /> },
-  { path: '/dashboard', element: <Dashboard /> },
-])
-
-// folosirea functiei create numita router pentru a chema paginile de mai sus
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/authentification" element={<AuthenticationPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/create-account" element={<CreateAccountPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
