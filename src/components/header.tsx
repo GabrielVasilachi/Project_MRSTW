add1739a7b384f338814b33c4968c8ab18c0f92e
Rostislav0805 <rostislavdudun474@gmail.com>
2026-02-15 20:28:28 +0200

    feat: added connection with button from header to pages login and signup

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,25 +5,29 @@ import Logo from '../../public/images/Logo.svg'
 export default function Header() {
     return (
         <header className="bg-white sticky shadow-md w-full z-40">
-        <nav className="container mx-auto px-4 py-4">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-3">
-                     <img src={Logo} alt="Logo" className="h-8 w-8" /> 
-                    <span className="text-xl font-bold text-gray-800">Portal Vamal</span>
-                </div>
-                
-                <div className="hidden md:flex items-center space-x-8">
-                    <Link to="/" className="text-gray-700 hover:text-amber-600 font-medium transition">Acasă</Link>
-                    <Link to="/guide" className="text-gray-700 hover:text-amber-600 font-medium transition">Cum funcționează</Link>
-                    <a href="#about" className="text-gray-700 hover:text-amber-600 font-medium transition">FAQ</a>
-                </div>
+            <nav className="container mx-auto px-4 py-4">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center space-x-3">
+                        <img src={Logo} alt="Logo" className="h-8 w-8" />
+                        <span className="text-xl font-bold text-gray-800">Portal Vamal</span>
+                    </div>
 
-                <div className="flex items-center space-x-4">
-                   <BaseButton variant="primary">Înregistrare</BaseButton>
-                   <BaseButton variant="secondary">Autentificare</BaseButton>
+                    <div className="hidden md:flex items-center space-x-8">
+                        <Link to="/" className="text-gray-700 hover:text-amber-600 font-medium transition">Acasă</Link>
+                        <Link to="/guide" className="text-gray-700 hover:text-amber-600 font-medium transition">Cum funcționează</Link>
+                        <a href="#about" className="text-gray-700 hover:text-amber-600 font-medium transition">FAQ</a>
+                    </div>
+
+                    <div className="flex items-center space-x-4">
+                        <Link to="/signup">
+                            <BaseButton variant="primary">Înregistrare</BaseButton>
+                        </Link>
+                        <Link to="/login">
+                            <BaseButton variant="secondary">Autentificare</BaseButton>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </header>
+            </nav>
+        </header>
     )
 }
