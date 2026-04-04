7f479eb6b23eba3c1bc1ffc7ffb0acbabfdf2035
negara999 <negara.sergiu2@gmail.com>
2026-04-04 13:30:39 +0300

    feat(domain): add DeclarationUpdateDto

---
+++ backend/MRSTW.Domain/Models/Declarations/DeclarationUpdateDto.cs
@@ -0,0 +1,7 @@
+namespace MRSTW.Domain.Models.Declarations;
+
+public class DeclarationUpdateDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
